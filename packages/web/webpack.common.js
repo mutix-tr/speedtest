f20b47f8e9e4a4c87e962e7c8b70c541d0dc90a3
Mutix <muti@mutix.tr>
2026-05-17 20:44:59 +0300

    chore: modernize webpack.common.js with ES2020 module support and caching

--- packages/web/webpack.common.js
+++ packages/web/webpack.common.js
@@ -16,7 +16,20 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "[name].[contenthash:8].js"
+    filename: "[name].[contenthash:8].js",
+    chunkFilename: "[name].[contenthash:8].chunk.js",
+    module: true,
+    libraryTarget: "module"
+  },
+  experiments: {
+    outputModule: true
+  },
+  cache: {
+    type: "filesystem",
+    cacheDirectory: path.resolve(__dirname, ".webpack_cache"),
+    buildDependencies: {
+      config: [__filename]
+    }
   },
   module: {
     rules: [
@@ -25,7 +38,8 @@ module.exports = {
         use: {
           loader: "worker-loader",
           options: {
-            name: "[name].[contenthash:8].js"
+            name: "[name].[contenthash:8].js",
+            esModule: true
           }
         }
       },
@@ -33,13 +47,19 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true
+          }
         }
       },
       {
         test: /views\/.*\.html$/,
         use: {
-          loader: "html-loader"
+          loader: "html-loader",
+          options: {
+            esModule: true
+          }
         }
       },
       {
@@ -54,17 +74,21 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: "url-loader",
-        options: {
-          limit: 1024,
-          name: "[name].[contenthash:8].[ext]"
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 1024
+          }
+        },
+        generator: {
+          filename: "[name].[contenthash:8][ext]"
         }
       },
       {
         test: /\.(eot|ttf|woff|woff2)$/,
-        loader: "file-loader",
-        options: {
-          name: "[name].[contenthash:8].[ext]"
+        type: "asset/resource",
+        generator: {
+          filename: "[name].[contenthash:8][ext]"
         }
       }
     ]
@@ -79,11 +103,30 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: "src/index.html",
-      chunks: ["app"]
+      chunks: ["app"],
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+        minifyCSS: true,
+        minifyJS: true
+      }
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash:8].css"
+      filename: "[name].[contenthash:8].css",
+      chunkFilename: "[name].[contenthash:8].chunk.css"
     }),
     new CopyWebpackPlugin([{ from: "src/assets/", to: "." }])
-  ]
+  ],
+  resolve: {
+    extensions: [".js", ".json"],
+    alias: {
+      "@app": path.resolve(__dirname, "src/js/app/"),
+      "@worker": path.resolve(__dirname, "src/js/worker/"),
+      "@utils": path.resolve(__dirname, "src/js/utils/")
+    }
+  }
 };
